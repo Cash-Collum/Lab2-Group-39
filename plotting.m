cfce53fb15fdaa4d96ee66b4caeee79fb13cb5a9
elyse-cabrera <elca1479@colorado.edu>
2025-10-07 13:00:33 -0600

    Add files via upload

--- plotting.m
+++ plotting.m
@@ -1,12 +1,13 @@
 function plotting(data,x0,channelLocations,name)
 
     steadyState = data(end,:);
-    distances = [x0,channelLocations];
+    distances = channelLocations;
 
     p = polyfit(distances,steadyState,1);
 
     x1 = linspace(0,channelLocations(end));
     y1 = polyval(p,x1);
+    T0 = polyval(p,x0);
 
     figure()
     scatter(distances,steadyState);
@@ -17,7 +18,8 @@ function plotting(data,x0,channelLocations,name)
     legend('Raw Data','Best Fit')
     title(name)
 
-    fprintf("For the %s case, T0 = %f \n",name,steadyState(1));
+    fprintf("For the %s case, T0 = %f \n",name,T0);
+    fprintf("For the %s case, H_exp = %f \n",name,p(1));
 
 
 end
