3f3beaaad6596ef49d801f0a165e4adafc0483fe
Cash-Collum <caco5421@colorado.edu>
2025-10-07 13:10:16 -0600

    Plotting

--- lab2_3802.m
+++ lab2_3802.m
@@ -34,7 +34,75 @@ plotting(Brass25V,x0,channelLocations,"Brass-25V-237mA");
 plotting(Brass30V,x0,channelLocations,"Brass-30V-285mA");
 plotting(Steel22V,x0,channelLocations,"Steel-22V-203mA");
 
+%%Find H_analytical
+Aluminumk = 130;
+Brassk = 115;
+Steelk = 16.2;
 
+AluminumV = [25, 30];
+BrassV = [25,30];
+SteelV = 22;
+
+AluminumI = [.240, .290];
+BrassI = [.237,.285];
+SteelI = .203;
+
+A = .0005067;
+
+HSteel = (SteelV .* SteelI) ./ (Steelk * A);
+HAluminum = (AluminumV .* AluminumI) ./ (Aluminumk * A);
+HBrass = (BrassV .* BrassI) ./ (Brassk * A);
+
+L = .2159;
+To = 14.539;
+B2T0 = 13.916;
+A1To = 15.983;
+A2To = 15.741;
+STo = 9.627;
+x = linspace(0, L, 10);
+xTherm = [.034925, 0.047625, 0.060325, 0.073025, 0.085725, 0.098425, 0.111125, 0.123825];
+
+
+
+figure();
+plot(x,HBrass(1) * x + To); hold on; grid on;
+scatter(xTherm, HBrass(1).*xTherm + To);
+title('Brass-25V-237mA');
+legend('Brass at 25 V and 237mA', 'Location of thermocouples');
+xlabel('Location(m)');
+ylabel('Temperature(deg C)');
+
+figure();
+plot(x,HBrass(2) * x + B2T0); hold on; grid on;
+scatter(xTherm, HBrass(2).*xTherm + B2T0);
+title('Brass-30V-285mA');
+legend('Brass at 30 V and 285mA', 'Location of thermocouples');
+xlabel('Location(m)');
+ylabel('Temperature(deg C)');
+
+figure();
+plot(x,HAluminum(1) * x + A1To); hold on; grid on;
+scatter(xTherm, HAluminum(1).*xTherm + A1To);
+title('Aluminum-25V-240mA');
+legend('Aluminum at 25 V and 240mA', 'Location of thermocouples');
+xlabel('Location(m)');
+ylabel('Temperature(deg C)');
+
+figure();
+plot(x,HAluminum(2) * x + A2To); hold on; grid on;
+scatter(xTherm, HAluminum(2).*xTherm + A2To);
+title('Aluminum-30V-290mA');
+legend('Aluminum at 30 V and 290mA', 'Location of thermocouples');
+xlabel('Location(m)');
+ylabel('Temperature(deg C)');
+
+figure();
+plot(x,HSteel * x + STo); hold on; grid on;
+scatter(xTherm, HSteel.*xTherm + STo);
+title('Steel-22V-203mA');
+legend('Steel at 25 V and 237mA', 'Location of thermocouples');
+xlabel('Location(m)');
+ylabel('Temperature(deg C)');
 
 
 
