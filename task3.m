b04d7ba2dcd26370f51a69fa45567a86ca4beaa2
MarioSolo10274 <maso7829@colorado.edu>
2025-11-03 14:58:05 -0700

    Add files via upload

--- task3.m
+++ task3.m
@@ -16,7 +16,7 @@ CaseNames = { ...
     'Brass 30V 285mA', ...
     'Steel 22V 203mA'};
 
-%% experimental data overlayed with H_an
+%% experimental data
 
 for j=1:length(Cases)
 
@@ -28,114 +28,94 @@ for j=1:length(Cases)
     hold on
     ylabel('Temperature (°C)')
     xlabel('Time (s)')
-    xlim([0 1000])
-    title(['H_{exp} over Experimental data for ', CaseNames{j}])
+    xlim([0 20000]) 
+    title(['H_{exp} over Experimental Data for ', CaseNames{j}])
     %for loop for experimental data
-    for i=2:8
+    for i=2:9
         Exper = Cases{j};
         plot(t,Exper(:,i),'b')
     end
     hold off
 end
 
-%% Task 1
-%t=1s
-N = 10;
+%% Constants
+N=50;
 L = .2413;
-x = .2159;
 H = 91.0871;
-T0 = 15.9830;
+T0 = [15.983,15.741,14.539,13.916,9.227];
 t = 1;
+x = 0.03495 + (0:7)*0.0254; 
 k_array = [130,115,16.2];
 ro_array = [2810,8500,8000];
 cp_array = [960,380,500];
-alpha = k / (ro * cp);
 total1 = 0;
 total2= 0;
-start = T0 + (H * x);
 TOTAL1 = zeros(1,N);
 TOTAL2 = zeros(1,N);
 lambda1 = zeros(1,N);
 bn1 = zeros(1,N);
 sum1 = zeros(1,N);
 
-for n = 1:N
-lambda1(n) = ((2*n - 1) * pi) / (2 * L);
-bn1(n) = ((-1)^(n) * (4*H*L)) / (2*n-1) * (2 / ((2*n-1) * pi * pi));
-sum1(n) = bn1(n) * sin(lambda1(n)*x) * exp(-lambda1(n)^2 * alpha * t);
-total1 = sum1(n) + total1;
-TOTAL1(n) = total1;
-end
+%% Task 3 overlay
 
-u1 = start + TOTAL1;
-
-%% Task 2 overlay
-
-%Model 1A
+%Model 1b
 
 lambda = zeros(1,N);
 bn = zeros(1,N);
 sum = zeros(1,N);
-tf = 1000;
-total = zeros(1,tf);
+tf = 10000;
 Total = zeros(1,tf);
 
-u = zeros(1,8);
-HAN = [91.0871,132.0763,101.61,146.7295,554.0675];
+HAN = [91.0871,132.0763,101.61,146.7295,554.0675]; %H_an for task 2
 HEXP = [55.399,78.553,104.987,150.169,287.308];
 
-for h = 1:length(HAN)
+for h = 1:length(HEXP)
+    if h<=2 
+        alpha = (k_array(1)/(ro_array(1)*cp_array(1)));
+    elseif h<=4
+        alpha = (k_array(2)/(ro_array(2)*cp_array(2)));
+    else
+        alpha = (k_array(3)/(ro_array(3)*cp_array(3)));
+    end
 
-        %different alpha values for each case/material
-        if h<=2 %aluminum alpha
-            alpha = (k_array(1)/(ro_array(1)*cp_array(1)));
-        elseif h<=4 %brass Alpha
-            alpha = (k_array(2)/(ro_array(2)*cp_array(2)));
-        else %steel alpha
-            alpha = (k_array(3)/(ro_array(3)*cp_array(3)));
-        end
+    H = HEXP(h);
+    T_0 = T0(h);
 
-start = T0 + (HAN(h) * x);
-START = T0 + (HEXP(h) * x);
 
-for x = linspace(.0381,L,8)
-    for t = 1:tf
+START = zeros(1,8);
+time = linspace(1,30000,30000);
+
+for q=1:8
+    START(q) = T_0 + (H * x(q));
+
+    for t = 1:length(time)
         TOTAL = 0;
-        TOtAL = 0;
         for n = 1:N
             lambda(n) = ((2*n - 1) * pi) / (2 * L);
-            bn(n) = ((-1)^(n) * (4*HAN(h)*L)) / (2*n-1) * (2 / ((2*n-1) * pi * pi));
-            sum(n) = bn(n) * sin(lambda(n)*x) * exp(-lambda(n)^2 * alpha * t);
+            bn(n) = ((-1)^(n) * (4*H*L)) / (2*n-1) * (2 / ((2*n-1) * pi * pi));
+            sum(n) = bn(n) * sin(lambda(n)*x(q)) * exp(-lambda(n)^2 * alpha * time(t));
             TOTAL = sum(n) + TOTAL;
-
-            Lambda = ((2*n - 1) * pi) / (2 * L);
-            Bn = ((-1)^(n) * (4*HEXP(h)*L)) / (2*n-1) * (2 / ((2*n-1) * pi * pi));
-            Sum = Bn * sin(Lambda*x) * exp(-Lambda^2 * alpha * t);
-            TOtAL = Sum + TOtAL;
         end
-        total(t) = TOtAL;
+      
         Total(t) = TOTAL;
     end
-%u = start + total;
-U = START + Total;
+U = START(q) + Total;
 
-time = linspace(1,1000,1000);
+
 
 %figure for H_exp
 figure(h)
 hold on
-plot(time,U, 'r');
+plot(time,U, 'r'); %Turn on if you want Original Overlay
 legend('Experimental Data','','','','','','','','H_{exp}','Location','best')
 hold off
-
-%saving each figure
-saveas(gcf, ['Hexp_Case_' CaseNames{h} '.png']);
-
-
+if h<=4
+    xlim([0 4000]);
+else
+    xlim([0 12000]);
+end
+    %saving each figure
+    saveas(gcf, ['Hexp_Case_' CaseNames{h} '.png']);
 end
 
 end
-
-
-
-
